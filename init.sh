6bc4985da391002faa10a239368016e486e2dde8
GusievVM <vitaliy.m.gusev@gmail.com>
2016-11-18 02:29:44 +0200

    Add all files to Git - 3

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-available/test.conf
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-#sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
