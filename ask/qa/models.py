a3eca086db4567193c47d83b35b45073ba1b2f83
GusievVM <vitaliy.m.gusev@gmail.com>
2016-11-25 17:34:11 +0200

    Step: 2.3.10. First try, to pass test.

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+import uuid
 
 # Create your models here.
 
+class QuestionManager(models.Manager):
 
+    def new(self):
+        qs = super(QuestionManager, self).get_queryset()
+        return qs.order_by('-id')
+
+    def popular(self):
+        qs = super(QuestionManager, self).get_queryset()
+        return qs.order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, related_name="question_author")
+    likes = models.ManyToManyField(User)
+
+    def get_url(self):
+        return '/question/{}/'.format(self.id)
+
+    def __unicode__(self):
+        return self.title
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User, related_name="answer_author")
+
+    def __unicode__(self):
+        return self.text
