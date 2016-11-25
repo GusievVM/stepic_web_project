fb65c367564f1aeba4439a5c8fbb60305a151b77
GusievVM <vitaliy.m.gusev@gmail.com>
2016-11-25 18:00:59 +0200

    Step: 2.3.10. Second try, to pass test.

--- init.sh
+++ init.sh
@@ -9,6 +9,8 @@
 
 
 
+# git clone https://github.com/GusievVM/stepic_web_project.git /home/box/web
+
 
 #sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-available/default
@@ -17,11 +19,15 @@ sudo ln -sf hello.py /usr/local/lib/python2.7/hello.py
 sudo mkdir /etc/gunicorn.d
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 
+sudo /etc/init.d/mysql restart﻿
+
 cd ~/web/
 sudo gunicorn -b 0.0.0.0:8080 hello:application &
 cd ~/web/ask/
 sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
 
-sudo /etc/init.d/mysql restart﻿
+
+
+
 
 #sudo /etc/init.d/gunicorn restart
